ca85cd9fd5f50c0bd233c18ba8b503956a898eee
anphan2410 <an.phan@ascenx.com>
2017-06-04 23:56:24 +0700

    update saved work

--- BWTrackerVSVB/Form1.vb
+++ BWTrackerVSVB/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Imports Excel = Microsoft.Office.Interop.Excel
-Imports Microsoft.Office
+Imports MSOffice = Microsoft.Office.Core
+Imports MSVBCollection = Microsoft.VisualBasic.Collection
 Public Class Form1
     Private ExcelFilePath As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) _
                                     & "\BWTracker.xlsx"
@@ -10,32 +11,127 @@ Public Class Form1
     Private BWTrackerWS As Excel.Worksheet
     Private KeyHierarchyWS As Excel.Worksheet
 
-    Private Function LastDataCell(ByRef AnyColumnRange As Excel.Range _
-            , Optional ByVal ExactMode As Boolean = False) As Excel.Range
-        Dim TmpWS As Excel.Worksheet = AnyColumnRange.Parent
-        Dim HiddenRowCollection As New Microsoft.VisualBasic.Collection()
-        If (ExactMode) Then
-            For i As UInteger = 1 To TmpWS.UsedRange.Rows.Count
-                If (TmpWS.Rows(i).EntireRow.Hidden) Then
-                    HiddenRowCollection.Add(Item:=i)
-                End If
-            Next
+    Private Sub StoreNumbersOfHiddenRowsIntoACollection(ASheet As Excel.Worksheet, ByRef DestinationCollection As MSVBCollection)
+        For i As Integer = 1 To ASheet.UsedRange.Rows.Count
+            If (ASheet.Rows(i).EntireRow.Hidden) Then
+                DestinationCollection.Add(Item:=i)
+            End If
+        Next
+    End Sub
+
+    Private Sub RestoreHiddenPropertyOfRowsFromACollection(ASheet As Excel.Worksheet, SourceCollection As MSVBCollection)
+        For Each AHiddenRow As VariantType In SourceCollection
+            ASheet.Rows(DirectCast(AHiddenRow, Integer)).EntireRow.Hidden = True
+        Next
+    End Sub
+
+    Private Sub StoreNumbersOfHiddenColumnsIntoACollection(ASheet As Excel.Worksheet, ByRef DestinationCollection As MSVBCollection)
+        For i As Integer = 1 To ASheet.UsedRange.Columns.Count
+            If (ASheet.Columns(i).EntireColumn.Hidden) Then
+                DestinationCollection.Add(Item:=i)
+            End If
+        Next
+    End Sub
+
+    Private Sub RestoreHiddenPropertyOfColumnsFromACollection(ASheet As Excel.Worksheet, SourceCollection As MSVBCollection)
+        For Each AHiddenColumn As VariantType In SourceCollection
+            ASheet.Columns(DirectCast(AHiddenColumn, Integer)).EntireColumn.Hidden = True
+        Next
+    End Sub
+
+    Private Function LastDataCellOfAColumn(ARangeOfASingleColumn As Excel.Range,
+                                           Optional ByVal PureDataMode As Boolean = False) As Excel.Range
+        Dim TmpWS As Excel.Worksheet = ARangeOfASingleColumn.Parent
+        Dim HiddenRowCollection As New MSVBCollection
+        If (PureDataMode) Then
+            StoreNumbersOfHiddenRowsIntoACollection(TmpWS, HiddenRowCollection)
             TmpWS.UsedRange.EntireRow.Hidden = False
         End If
-        Dim FullColRng As Excel.Range = TmpWS.UsedRange.Columns(AnyColumnRange.Column)
-        Dim TmpDataCell As Excel.Range = FullColRng.Range("A" & FullColRng.Rows.Count).End(Excel.XlDirection.xlUp)
-        If (ExactMode) Then
-            For Each AHiddenRow As VariantType In HiddenRowCollection
-                TmpWS.Rows(DirectCast(AHiddenRow, Integer)).EntireRow.Hidden = True
-            Next
+        Dim FullColRng As Excel.Range = TmpWS.UsedRange.Columns(ARangeOfASingleColumn.Column)
+        LastDataCellOfAColumn = FullColRng.Range("A" & (FullColRng.Rows.Count + 1)).End(Excel.XlDirection.xlUp)
+        If (PureDataMode) Then
+            RestoreHiddenPropertyOfRowsFromACollection(TmpWS, HiddenRowCollection)
         End If
+        HiddenRowCollection.Clear()
         HiddenRowCollection = Nothing
-        LastDataCell = TmpDataCell
+    End Function
+
+    Private Function LastDataCellOfARow(ARangeOfASingleRow As Excel.Range,
+                                        Optional ByVal PureDataMode As Boolean = False) As Excel.Range
+        Dim TmpWS As Excel.Worksheet = ARangeOfASingleRow.Parent
+        Dim HiddenColumnCollection As New MSVBCollection
+        If (PureDataMode) Then
+            StoreNumbersOfHiddenColumnsIntoACollection(TmpWS, HiddenColumnCollection)
+            TmpWS.UsedRange.EntireColumn.Hidden = False
+        End If
+        LastDataCellOfARow = DirectCast(TmpWS.Cells(ARangeOfASingleRow.Row,
+                                                    TmpWS.UsedRange.Columns.Count + 1),
+                                                    Excel.Range).
+                                                    End(Excel.XlDirection.xlToLeft)
+        If (PureDataMode) Then
+            RestoreHiddenPropertyOfColumnsFromACollection(TmpWS, HiddenColumnCollection)
+        End If
+        HiddenColumnCollection.Clear()
+        HiddenColumnCollection = Nothing
+    End Function
+
+    Private Function HeaderCell(ASheet As Excel.Worksheet,
+                                Optional UniqueKey As String = Nothing,
+                                Optional IsBold As Boolean = True,
+                                Optional AccuracyPercent As Double = 1,
+                                Optional PureDataMode As Boolean = False) As Excel.Range
+        Dim HiddenRowCollection As New MSVBCollection
+        Dim HiddenColumnCollection As New MSVBCollection
+        If (PureDataMode) Then
+            StoreNumbersOfHiddenRowsIntoACollection(ASheet, HiddenRowCollection)
+            StoreNumbersOfHiddenColumnsIntoACollection(ASheet, HiddenColumnCollection)
+            ASheet.UsedRange.EntireRow.Hidden = False
+            ASheet.UsedRange.EntireColumn.Hidden = False
+        End If
+        If ((UniqueKey IsNot Nothing) And (UniqueKey <> "")) Then
+            HeaderCell = ASheet.UsedRange.Find(UniqueKey,
+                                               LookIn:=Excel.XlFindLookIn.xlValues,
+                                               SearchOrder:=Excel.XlSearchOrder.xlByRows,
+                                               SearchDirection:=Excel.XlSearchDirection.xlNext,
+                                               MatchCase:=True)
+        Else
+            Dim LastDataRowOfFirstColumn As Integer = LastDataCellOfAColumn(ASheet.Columns(1)).Row
+            Dim maxNumbersOfBoldCellsInARow As Integer = 1
+            Dim tmpHeaderRowNumber As Integer = LastDataRowOfFirstColumn
+            Dim PercentMatched As Double = 0
+            If (IsBold) Then
+                For i As Integer = 1 To LastDataRowOfFirstColumn Step 1
+                    If (DirectCast(ASheet.Cells(i, 1), Excel.Range).Font.Bold) Then
+                        Dim count As Integer = 1
+                        Dim LastDataColumnOfCurrentRow = LastDataCellOfARow(ASheet.Rows(i)).Column
+                        For j As Integer = 2 To LastDataColumnOfCurrentRow Step 1
+                            If (DirectCast(ASheet.Cells(i, j), Excel.Range).Font.Bold) Then
+                                count += 1
+                            End If
+                        Next
+                        PercentMatched = count / LastDataColumnOfCurrentRow
+                        If (PercentMatched >= AccuracyPercent) Then
+                            HeaderCell = DirectCast(ASheet.Cells(i, 1), Excel.Range)
+                            Exit Function
+                        End If
+                        If (count > maxNumbersOfBoldCellsInARow) Then
+                            maxNumbersOfBoldCellsInARow = count
+                            tmpHeaderRowNumber = i
+                        End If
+                    End If
+                Next
+                HeaderCell = DirectCast(ASheet.Cells(tmpHeaderRowNumber, 1), Excel.Range)
+            End If
+        End If
+        If (PureDataMode) Then
+            RestoreHiddenPropertyOfRowsFromACollection(ASheet, HiddenRowCollection)
+            RestoreHiddenPropertyOfColumnsFromACollection(ASheet, HiddenColumnCollection)
+        End If
     End Function
 
     Private Sub ButtonClearTextBoxTaskDescription_Click(sender As Object, e As EventArgs) Handles ButtonClearTextBoxTaskDescription.Click
-        LastDataCell(KeyHierarchyWS.Columns(2), True).Value = "test"
-
+        'MsgBox(KeyHierarchyWS.UsedRange.Find(TextBoxTaskDescription.Text, LookIn:=Excel.XlFindLookIn.xlValues).Address)
+        MsgBox(HeaderCell(BWTrackerWS).Address)
         TextBoxTaskDescription.Clear()
     End Sub
 
@@ -112,12 +208,13 @@ Public Class Form1
         End While
         BWTrackerWS = BWTrackerWB.Worksheets(BWTrackerWSName)
         KeyHierarchyWS = BWTrackerWB.Worksheets(KeyHierarchyWSName)
-        'Dim KeyRng As Excel.Range = KeyHierarchyWS.Ra
+        Dim UniqueKeyRng As Excel.Range
+        Dim ParentKeyRng As Excel.Range
         Dim TaskPathSource As New AutoCompleteStringCollection()
-        TaskPathSource.Add("AscenX")
-        TaskPathSource.Add("AscenX/TamIoT")
-        TaskPathSource.Add("AscenX/HieuCMS")
-        TaskPathSource.Add("AscenX/SonSETraining")
+        'TaskPathSource.Add("AscenX")
+        'TaskPathSource.Add("AscenX/TamIoT")
+        'TaskPathSource.Add("AscenX/HieuCMS")
+        'TaskPathSource.Add("AscenX/SonSETraining")
         TextBoxTaskPath.AutoCompleteCustomSource = TaskPathSource
         TextBoxTaskPath.AutoCompleteMode = AutoCompleteMode.SuggestAppend
         TextBoxTaskPath.AutoCompleteSource = AutoCompleteSource.CustomSource
